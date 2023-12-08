28dce03c6b38f2913e665e99994687347471013f
“Maeve <melf.mayer@studium.uni-hamburg.de>
2023-12-08 17:22:48 +0100

    modified grid for internal wave

--- for_config/internal_wave1.f90
+++ for_config/internal_wave1.f90
@@ -1,11 +1,11 @@
 
-  
+
 !=======================================================================
 ! Internal wave maker
 !=======================================================================
 
 module config_module
- use main_module   
+ use main_module
  real*8 :: fac=2.0
  real*8 :: N_0 = 2*pi/10.
  real*8 :: OM0 = 1./(1.5*10),x0
@@ -13,10 +13,10 @@ module config_module
 end module config_module
 
 subroutine set_parameter
- use main_module   
- use config_module   
- use diagnostics_module   
- use tke_module   
+ use main_module
+ use config_module
+ use diagnostics_module
+ use tke_module
  implicit none
   ny=1
   nx=int(64*fac)
@@ -24,7 +24,7 @@ subroutine set_parameter
 
   dt_mom   = 20*0.025/fac
   dt_tracer= 20*0.025/fac
-     
+
   enable_conserve_energy = .false.
   coord_degree           = .false.
   enable_cyclic_x        = .true.
@@ -34,10 +34,10 @@ subroutine set_parameter
   congr_epsilon = 1e-12
   congr_max_iterations = 5000
   congr_epsilon_non_hydro=   1e-9
-  congr_max_itts_non_hydro = 5000    
+  congr_max_itts_non_hydro = 5000
 
   enable_explicit_vert_friction = .true.
-  kappam_0 = 5e-3/fac**2   
+  kappam_0 = 5e-3/fac**2
   enable_hor_friction =  .true.
   a_h      = 5e-3/fac**2
   enable_superbee_advection = .true.
@@ -53,24 +53,24 @@ end subroutine set_parameter
 
 
 subroutine set_grid
- use main_module   
- use config_module   
+ use main_module
+ use config_module
  implicit none
- dxt(:)=0.25/fac 
- dyt(:)=0.25/fac 
- dzt(:)=0.25/fac 
+ dxt(:)=0.25/fac
+ dyt(:)=0.25/fac
+ dzt(:)=0.25/fac
 end subroutine set_grid
 
 
 
 subroutine set_initial_conditions
-  use main_module   
-  use config_module   
+  use main_module
+  use config_module
   implicit none
   integer :: i,k
   real*8 :: alpha,get_drhodT
 
-  alpha = get_drhodT(35d0,5d0,0d0) 
+  alpha = get_drhodT(35d0,5d0,0d0)
   allocate( t0(is_pe-onx:ie_pe+onx,js_pe-onx:je_pe+onx,nz) ); t0 = 0
   allocate( u0(is_pe-onx:ie_pe+onx,js_pe-onx:je_pe+onx,nz) ); u0 = 0
   allocate( dt0(is_pe-onx:ie_pe+onx,js_pe-onx:je_pe+onx,nz,3) ); dt0 = 0
@@ -92,32 +92,38 @@ end subroutine set_initial_conditions
 
 subroutine set_forcing
  use main_module
-  use config_module   
+  use config_module
  implicit none
 
  ! implement effect of background state
- 
+
  ! update density, etc of last time step
  temp(:,:,:,tau) = temp(:,:,:,tau) + t0
  call calc_eq_of_state(tau)
  temp(:,:,:,tau) = temp(:,:,:,tau) - t0
-      
+
  ! advection of background temperature
- call advect_tracer(is_pe-onx,ie_pe+onx,js_pe-onx,je_pe+onx,nz,t0,dt0(:,:,:,tau)) 
+ call advect_tracer(is_pe-onx,ie_pe+onx,js_pe-onx,je_pe+onx,nz,t0,dt0(:,:,:,tau))
  temp_source = (1.5+ab_eps)*dt0(:,:,:,tau) - ( 0.5+ab_eps)*dt0(:,:,:,taum1)
 
  ! wavemaker
  u_source= u0*sin(2*pi*OM0*itt*dt_tracer)
- 
+
 end subroutine set_forcing
 
 subroutine set_coriolis
 end subroutine set_coriolis
 
 subroutine set_topography
- use main_module   
+ use main_module
  implicit none
  kbot =1
+ alpha = 30*pi/180
+ do i=is_pe,ie_pe
+   do j=js_pe,je_pe
+     if (i>=nx/2.and.j<=(i-nx/2)*tan(alpha))  kbot(i,j)=0
+   enddo
+ enddo
 end subroutine set_topography
 
 
@@ -127,6 +133,3 @@ end subroutine set_diagnostics
 
 subroutine set_particles
 end subroutine set_particles
-
-
-
