ae53684fbebb5fe6eedc773c45901f5b66162132
“Maeve <melf.mayer@studium.uni-hamburg.de>
2023-12-11 16:22:49 +0100

    added specific files

--- for_config/internal_wave_critical.f90
+++ for_config/internal_wave_critical.f90
@@ -45,7 +45,7 @@ subroutine set_parameter
   enable_tempsalt_sources = .true.
   enable_momentum_sources = .true.
 
-  runlen = 864.0 !86400.0
+  runlen = 86400.0 !86400.0
   enable_diag_ts_monitor = .true.; ts_monint = 0.5!dt_mom
   enable_diag_snapshots  = .true.; snapint  = 5.0!dt_mom
 
