e69f2c2e74e6cf2b6608e81df345df73c7ebed7c
“Maeve <melf.mayer@studium.uni-hamburg.de>
2023-12-11 15:31:10 +0100

    bug?

--- for_config/internal_wave1.f90
+++ for_config/internal_wave1.f90
@@ -116,13 +116,12 @@ end subroutine set_coriolis
 
 subroutine set_topography
  use main_module
- use config_module
  implicit none
  integer :: i,k
  real*8 :: alpha
  kbot = 1
  alpha = 30*pi/180
- !do i=is_pe-onx,ie_pe+onx
+! do i=is_pe-onx,ie_pe+onx
 !   do k=1,nz
 !     if ((i-nx/2)*tan(alpha)>=k) kbot(i,:)=k
 !   enddo
